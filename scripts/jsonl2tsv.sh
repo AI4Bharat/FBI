6623569864c1c8dc057b1087816ba5367fbe9da6
Sumanth Doddapaneni <dsumanth17@gmail.com>
2024-05-21 12:38:11 +0530

    helper scripts

---
+++ scripts/jsonl2tsv.sh
@@ -0,0 +1,7 @@
+python scripts/jsonl2tsv.py --file_name contextual-errors-answers.jsonl
+python scripts/jsonl2tsv.py --file_name entity-errors-answers.jsonl
+python scripts/jsonl2tsv.py --file_name incorrect-fact-answers.jsonl
+python scripts/jsonl2tsv.py --file_name number-errors-answers.jsonl
+python scripts/jsonl2tsv.py --file_name opposite-facts-answers.jsonl
+python scripts/jsonl2tsv.py --file_name remove-facts-answers.jsonl
+python scripts/jsonl2tsv.py --file_name multiple-direct-errors-answers.jsonl
---
+++ scripts/run_parser.sh
@@ -0,0 +1,7 @@
+python -m scripts.run_parser --file_name contextual-errors-answers.tsv --parser direct_error
+python -m scripts.run_parser --file_name entity-errors-answers.tsv --parser direct_error
+python -m scripts.run_parser --file_name incorrect-fact-answers.tsv --parser direct_error
+python -m scripts.run_parser --file_name number-errors-answers.tsv --parser direct_error
+python -m scripts.run_parser --file_name opposite-facts-answers.tsv --parser direct_error
+python -m scripts.run_parser --file_name remove-facts-answers.tsv --parser direct_error
+python -m scripts.run_parser --file_name multiple-direct-errors-answers.tsv --parser direct_error
