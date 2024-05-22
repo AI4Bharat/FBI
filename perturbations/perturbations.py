a60f180e39609947ec38d97d4cef05675959efd3
Sumanth Doddapaneni <dsumanth17@gmail.com>
2024-05-22 12:36:59 +0530

    long form perturbations. todo: clean up

--- perturbations/perturbations.py
+++ perturbations/perturbations.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 import argparse
 import pandas as pd
 from ast import literal_eval
@@ -25,6 +26,8 @@ def parse_args():
     parser.add_argument('--facts', action='store_true', help='Generate factual statements')
     parser.add_argument('--errors', action='store_true', help='Generate factual statements with errors')
     parser.add_argument('--stitch', action='store_true', help='Stitch factual statements with errors')
+    # debug
+    parser.add_argument('--debug', action='store_true', help='Debug mode')
     args = parser.parse_args()
     return args
 
@@ -55,7 +58,10 @@ def create_jsonl(cdx: str, model_name: str, prompt: str, max_tokens: int, temper
     }
 
 
-def dump_jsonl(jsons: list, file_name: str) -> None:
+def dump_jsonl(args: argparse.Namespace, jsons: list, file_name: str) -> None:
+    if args.debug:
+        jsons = random.sample(jsons, 20)
+    
     with open(file_name, 'w') as f:
         for json_ in jsons:
             f.write(json.dumps(json_) + '\n')
@@ -118,7 +124,7 @@ def factual_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> No
         dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
         jsons.append(dict_)
 
-    dump_jsonl(jsons, f'{args.data_dir}/{save_name}.jsonl')
+    dump_jsonl(args, jsons, f'{args.data_dir}/{save_name}.jsonl')
     return
         
 
@@ -149,7 +155,7 @@ def factual_perturbations_v2(args: argparse.Namespace, testset: pd.DataFrame) ->
         dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
         jsons.append(dict_)
     
-    dump_jsonl(jsons, f'{args.data_dir}/direct-errors-temp{args.temperature}.jsonl')
+    dump_jsonl(args, jsons, f'{args.data_dir}/direct-errors-temp{args.temperature}.jsonl')
     return
 
 
@@ -180,7 +186,7 @@ def factual_perturbations_v3(args: argparse.Namespace, testset: pd.DataFrame) ->
         dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
         jsons.append(dict_)
     
-    dump_jsonl(jsons, f'{args.data_dir}/multiple-direct-errors-temp{args.temperature}.jsonl')
+    dump_jsonl(args, jsons, f'{args.data_dir}/multiple-direct-errors-temp{args.temperature}.jsonl')
     return
 
 
@@ -216,7 +222,7 @@ def contextual_fact_perturbations(args: argparse.Namespace, testset: pd.DataFram
         dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
         jsons.append(dict_)
     
-    dump_jsonl(jsons, f'{args.data_dir}/contextual-errors-temp{args.temperature}.jsonl')
+    dump_jsonl(args, jsons, f'{args.data_dir}/contextual-errors-temp{args.temperature}.jsonl')
     return
 
 
@@ -249,7 +255,7 @@ def number_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> Non
         dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
         jsons.append(dict_)
 
-    dump_jsonl(jsons, f'{args.data_dir}/number-errors-temp{args.temperature}.jsonl')
+    dump_jsonl(args, jsons, f'{args.data_dir}/number-errors-temp{args.temperature}.jsonl')
     return
 
 
@@ -282,7 +288,7 @@ def entity_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> Non
         dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
         jsons.append(dict_)
 
-    dump_jsonl(jsons, f'{args.data_dir}/entity-errors-temp{args.temperature}.jsonl')
+    dump_jsonl(args, jsons, f'{args.data_dir}/entity-errors-temp{args.temperature}.jsonl')
     return
 
 
@@ -315,7 +321,7 @@ def add_incorrect_fact(args: argparse.Namespace, testset: pd.DataFrame) -> None:
         dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
         jsons.append(dict_)
     
-    dump_jsonl(jsons, f'{args.data_dir}/incorrect-fact-temp{args.temperature}.jsonl')
+    dump_jsonl(args, jsons, f'{args.data_dir}/incorrect-fact-temp{args.temperature}.jsonl')
     return
 
 
@@ -348,7 +354,7 @@ def opposite_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> N
         dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
         jsons.append(dict_)
     
-    dump_jsonl(jsons, f'{args.data_dir}/opposite-fact-temp{args.temperature}.jsonl')
+    dump_jsonl(args, jsons, f'{args.data_dir}/opposite-fact-temp{args.temperature}.jsonl')
     return
 
 
@@ -381,7 +387,288 @@ def remove_fact(args: argparse.Namespace, testset: pd.DataFrame) -> None:
         dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
         jsons.append(dict_)
     
-    dump_jsonl(jsons, f'{args.data_dir}/remove-fact-temp{args.temperature}.jsonl')
+    dump_jsonl(args, jsons, f'{args.data_dir}/remove-fact-temp{args.temperature}.jsonl')
+    return
+
+
+def grammar_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> None:
+    """Given a gold answer, introduce grammatical errors.
+    
+    Args:
+        args (argparse.Namespace): Arguments
+        testset (pd.DataFrame): Testset
+        
+    Returns:
+        None
+    """
+    jsons = []
+    for _, row in testset.iterrows():
+        parser = JsonOutputParser(pydantic_object=DirectError)
+        PROMPT =  (
+            "Given the following Gold Answer, introduce a grammatical error in the answer.\n"
+            "Here are a list of possible errors you can introduce:\n"
+            "1. Subject-Verb Agreement Errors\n"
+            "2. Pronoun-Antecedent Agreement Errors\n"
+            "3. Misplaced Modifiers\n"
+            "4. Dangling Modifiers\n"
+            "5. Run-On Sentences\n"
+            "6. Comma Splices\n"
+            "7. Incorrect Use of Apostrophes\n"
+            "8. Confusion Between 'Its' and 'It's'\n"
+            "9. Incorrect Verb Tense\n"
+            "10. Fragmented Sentences\n"
+            "11. Double Negatives\n"
+            "12. Incorrect Word Usage\n"
+            "13. Confusion Between 'Your' and 'You're'\n"
+            "14. Confusion Between 'Then' and 'Than'\n"
+            "15. Preposition at the End of a Sentence\n"
+            "16. Incorrect Pluralization\n\n"
+            "Pick one of the errors from the above list and introduce it in the answer.\n"
+            "And make this error consistent throughout the answer.\n"
+            f"{parser.get_format_instructions()}\n\n"
+            "Gold Answer:\n"
+            f"{row['answer']}\n\n"
+            "Rewrite the full Gold Answer with the introduced error.\n"
+        )
+        dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
+        jsons.append(dict_)
+    
+    dump_jsonl(args, jsons, f'{args.data_dir}/grammar-errors-temp{args.temperature}.jsonl')
+    return
+
+
+def spelling_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> None:
+    """Given a gold answer, introduce spelling errors.
+    
+    Args:
+        args (argparse.Namespace): Arguments
+        testset (pd.DataFrame): Testset
+
+    Returns:
+        None
+    """
+    jsons = []
+    for _, row in testset.iterrows():
+        parser = JsonOutputParser(pydantic_object=DirectError)
+        PROMPT = (
+            "Given the following Gold Answer, introduce a spelling error in the answer.\n"
+            "Make sure that the introduced error is not immediately obvious and requires some level of knowledge to identify.\n"
+            "Please make sure that the introduced error is consistent throughout the answer.\n"
+            "Provide an explanation of the introduced error.\n"
+            f"{parser.get_format_instructions()}\n\n"
+            "Gold Answer:\n"
+            f"{row['answer']}\n\n"
+            "Rewrite the full Gold Answer with the introduced error.\n"
+        )
+        dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
+        jsons.append(dict_)
+
+    dump_jsonl(args, jsons, f'{args.data_dir}/spelling-errors-temp{args.temperature}.jsonl')
+    return
+
+
+def chronological_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> None:
+    """Given a gold answer, introduce chronological errors.
+    
+    Args:
+        args (argparse.Namespace): Arguments
+        testset (pd.DataFrame): Testset
+        
+    Returns:
+        None
+    """
+    jsons = []
+    for _, row in testset.iterrows():
+        parser = JsonOutputParser(pydantic_object=DirectError)
+        PROMPT = (
+            "Given the following Gold Answer, introduce chronological errors in the answer.\n"
+            "Mix up the sequence of events, use incorrect time references, switch between past, present, and future tenses inappropriately, and introduce conflicting timelines to create inconsistencies in the order of actions.\n"
+            "Provide an explanation of the introduced error.\n"
+            f"{parser.get_format_instructions()}\n\n"
+            "Gold Answer:\n"
+            f"{row['answer']}\n\n"
+            "Rewrite the full Gold Answer with the introduced error.\n"
+        )
+        dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
+        jsons.append(dict_)
+    
+    dump_jsonl(args, jsons, f'{args.data_dir}/chronological-errors-temp{args.temperature}.jsonl')
+    return
+
+
+def consistency_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> None:
+    """Given a gold answer, introduce inconsistencies in the answer.
+    
+    Args:
+        args (argparse.Namespace): Arguments
+        testset (pd.DataFrame): Testset
+    
+    Returns:
+        None
+    """
+    jsons = []
+    for _, row in testset.iterrows():
+        parser = JsonOutputParser(pydantic_object=DirectError)
+        # removed: conflicting information, contradictory facts, and
+        PROMPT = (
+            "Given the following Gold Answer, introduce inconsistencies in the answer.\n"
+            "Introduce logical inconsistencies to create errors in the answer.\n"
+            "Provide an explanation of the introduced error.\n"
+            f"{parser.get_format_instructions()}\n\n"
+            "Gold Answer:\n"
+            f"{row['answer']}\n\n"
+            "Rewrite the full Gold Answer with the introduced error.\n"
+        )
+        dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
+        jsons.append(dict_)
+
+    dump_jsonl(args, jsons, f'{args.data_dir}/consistency-errors-temp{args.temperature}.jsonl')
+    return
+
+
+def coherence_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> None:
+    """Given a gold answer, introduce coherence errors.
+    
+    Args:
+        args (argparse.Namespace): Arguments
+        testset (pd.DataFrame): Testset
+    
+    Returns:
+        None
+    """
+    jsons = []
+    for _, row in testset.iterrows():
+        parser = JsonOutputParser(pydantic_object=DirectError)
+        # todo: blend the errors into the answer
+        PROMPT = (
+            "Given the following Gold Answer, introduce coherence errors in the answer.\n"
+            "Introduce disjointed ideas, disconnected thoughts, and fragmented information to create errors in the answer.\n"
+            "Provide an explanation of the introduced error.\n"
+            f"{parser.get_format_instructions()}\n\n"
+            "Gold Answer:\n"
+            f"{row['answer']}\n\n"
+            "Rewrite the full Gold Answer with the introduced error.\n"
+        )
+        dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
+        jsons.append(dict_)
+
+    dump_jsonl(args, jsons, f'{args.data_dir}/coherence-errors-temp{args.temperature}.jsonl')
+    return
+
+
+def formatting_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> None:
+    """Given a gold answer, remove the formatting in the answers. This is a score invariant perturbation.
+    
+    Args:
+        args (argparse.Namespace): Arguments
+        testset (pd.DataFrame): Testset
+    
+    Returns:
+        None
+    """
+    jsons = []
+    for _, row in testset.iterrows():
+        parser = JsonOutputParser(pydantic_object=DirectError)
+        PROMPT = (
+            "Given the following Gold Answer, remove the formatting in the answer.\n"
+            "Except newline characters remove any bold, italic, underline, or any other formatting in the answer.\n"
+            "Do not change any logical or factual information in the answer. Only change the formatting.\n"
+            "Provide an explanation of the introduced error.\n"
+            f"{parser.get_format_instructions()}\n\n"
+            "Gold Answer:\n"
+            f"{row['answer']}\n\n"
+            "Rewrite the full Gold Answer with the introduced error.\n"
+        )
+        dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
+        jsons.append(dict_)
+
+    dump_jsonl(args, jsons, f'{args.data_dir}/formatting-errors-temp{args.temperature}.jsonl')
+    return
+
+
+def comprehensiveness_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> None:
+    """Given a Gold Answer, introduce errors that make the answer less comprehensive.
+    
+    Args:
+        args (argparse.Namespace): Arguments
+        testset (pd.DataFrame): Testset
+    
+    Returns:
+        None
+    """
+    jsons = []
+    for _, row in testset.iterrows():
+        parser = JsonOutputParser(pydantic_object=DirectError)
+        # TODO: errors are too short answers
+        PROMPT = (
+            "Given the following Question and Gold Answer, introduce one of the below errors.\n"
+            "1. Lack of Detail\n"
+            "2. Vagueness\n"
+            "3. Irrelevance\n"
+            "4. Incomplete Information\n"
+            "5. Poor Structure\n"
+            "6. Lack of Examples\n"
+            "7. Inaccuracies\n"
+            "8. Lack of Context\n"
+            "9. Unclear Language\n"
+            "10. Absence of Concluding Remarks\n"
+            "11. Failure to Address All Parts of the Question\n\n"
+            "Pick one of the errors from the above list and introduce it in the answer.\n"
+            "Do not drastically change the answer, only introduce an error that makes the answer less comprehensive with respect to the given question.\n"
+            "Provide an explanation of the introduced error.\n"
+            f"{parser.get_format_instructions()}\n\n"
+            "Question:\n"
+            f"{row['question']}\n\n"
+            "Gold Answer:\n"
+            f"{row['answer']}\n\n"
+            "Rewrite the full Gold Answer with the introduced error.\n"
+        )
+        dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
+        jsons.append(dict_)
+
+    dump_jsonl(args, jsons, f'{args.data_dir}/comprehensiveness-errors-temp{args.temperature}.jsonl')
+    return
+
+
+def superficial_perturbations(args: argparse.Namespace, testset: pd.DataFrame) -> None:
+    """Given a Gold Answer, introduce superficial cues to make it look like LLM generated answers.
+    
+    Args:
+        args (argparse.Namespace): Arguments
+        testset (pd.DataFrame): Testset
+    
+    Returns:
+        None
+    """
+    jsons = []
+    for _, row in testset.iterrows():
+        parser = JsonOutputParser(pydantic_object=DirectError)
+        PROMPT = (
+            "Given the following Gold Answer, introduce a factual error in the answers and also introduce superficial cues to make it look like a Language Model generated answer.\n"
+            "These could include any of the below errors that do not add any value to the answer.\n"
+            "1. Redundancy\n"
+            "2. Filler Words and Phrases\n"
+            "3. Overly Long Sentences\n"
+            "4. Unrelated Information\n"
+            "5. Excessive Qualifiers\n"
+            "6. Repetitive Phrasing\n"
+            "7. Verbose Introductions and Conclusions\n"
+            "8. Unnecessary Examples\n"
+            "9. Generalizations\n"
+            "10. Abstract Language\n"
+            "11. Parenthetical Statements\n"
+            "12. Unnecessary Citations or References\n\n"
+            "Pick one of the errors from the above list and introduce it in the answer.\n"
+            "Provide an explanation of the introduced error.\n"
+            f"{parser.get_format_instructions()}\n\n"
+            "Gold Answer:\n"
+            f"{row['answer']}\n\n"
+            "Rewrite the full Gold Answer with the introduced error.\n"
+        )
+        dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
+        jsons.append(dict_)
+
+    dump_jsonl(args, jsons, f'{args.data_dir}/superficial-errors-temp{args.temperature}.jsonl')
     return
 
 
@@ -405,7 +692,14 @@ def main(args):
     elif args.subset == 'instruction-following':
         pass
     elif args.subset == 'long-form':
-        pass
+        # grammar_perturbations(args, testset)
+        # spelling_perturbations(args, testset)
+        # chronological_perturbations(args, testset)
+        # consistency_perturbations(args, testset)
+        # coherence_perturbations(args, testset)
+        # formatting_perturbations(args, testset)
+        # comprehensiveness_perturbations(args, testset)
+        superficial_perturbations(args, testset)
     else:
         raise ValueError('Invalid subset')
 
--- scripts/jsonl2tsv.sh
+++ scripts/jsonl2tsv.sh
@@ -1,7 +1,15 @@
-python scripts/jsonl2tsv.py --file_name contextual-errors-answers.jsonl
-python scripts/jsonl2tsv.py --file_name entity-errors-answers.jsonl
-python scripts/jsonl2tsv.py --file_name incorrect-fact-answers.jsonl
-python scripts/jsonl2tsv.py --file_name number-errors-answers.jsonl
-python scripts/jsonl2tsv.py --file_name opposite-facts-answers.jsonl
-python scripts/jsonl2tsv.py --file_name remove-facts-answers.jsonl
-python scripts/jsonl2tsv.py --file_name multiple-direct-errors-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name contextual-errors-answers-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name entity-errors-answers-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name incorrect-fact-answers-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name number-errors-answers-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name opposite-facts-answers-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name remove-facts-answers-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name multiple-direct-errors-answers-temp1.0-answers.jsonl
+
+# python scripts/jsonl2tsv.py --file_name consistency-errors-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name coherence-errors-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name formatting-errors-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name comprehensiveness-errors-temp1.0-answers.jsonl
+# python scripts/jsonl2tsv.py --file_name comprehensiveness-errors-temp1.0-answers2.jsonl
+# python scripts/jsonl2tsv.py --file_name superficial-errors-temp1.0-answers.jsonl
+python scripts/jsonl2tsv.py --file_name superficial-errors-temp1.0-answers2.jsonl
