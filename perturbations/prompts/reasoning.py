4fa826e5c3f59716fc9bda82df44937bfa4dfd76
safikhanSoofiyani <safikhan2000@gmail.com>
2024-05-24 14:57:37 +0530

    added wrong-formula v2

--- perturbations/prompts/reasoning.py
+++ perturbations/prompts/reasoning.py
@@ -195,6 +195,37 @@ def wrong_formula(args: argparse.Namespace, testset: pd.DataFrame) -> None:
     dump_jsonl(args, jsons, f'{args.data_dir}/wrong-formula-temp{args.temperature}.jsonl')
     return
 
+def wrong_formula_v2(args: argparse.Namespace, testset: pd.DataFrame) -> None:
+    """Given the following answer for a reasoning question, change the formula.
+    
+    Args:
+        args (argparse.Namespace): Command line arguments
+        testset (pd.DataFrame): Testset
+    
+    Returns:
+        None
+    """
+    jsons = []
+    for _, row in testset.iterrows():
+        parser = JsonOutputParser(pydantic_object=DirectError)
+        PROMPT = (
+            "I want to create a few questions to test how good my evaluations system is. For this, I have collected some reasoning questions along with their correct solutions i.e., gold answers."
+            "For testing my evaluators, I want to make some subtle errors in the formulae used in the solution to see if it is being detected by the system or not.\n"
+            "Given a question and a solution (gold answer), I want you to help me in creating such errored solutions. You will only make subtle changes in some formula used in the solution that will render the solution incorrect while not changing any other step including the final answer.\n\n"    
+            "Make sure the final answer is unchanged, only introduce errors in the formulae of the solution.\n"
+            "Question:\n"
+            f"{row['question']}\n\n"
+            "Gold Answer:\n"
+            f"{row['answer']}\n\n"
+            "Rewrite the full Gold Answer with the introduced error.\n"
+            f"{parser.get_format_instructions()}\n\n"
+        )
+        dict_ = create_jsonl(row['cdx'], args.model, PROMPT, args.max_tokens, args.temperature, args.top_p, args.frequency_penalty, args.presence_penalty)
+        jsons.append(dict_)
+
+    dump_jsonl(args, jsons, f'{args.data_dir}/wrong-formula-temp{args.temperature}.jsonl')
+    return
+
 
 def copying_numbers_errors(args: argparse.Namespace, testset: pd.DataFrame) -> None:
     """Given the following reasoning question and it's answer, introduce errors in copying the numbers from the question to the answer.
