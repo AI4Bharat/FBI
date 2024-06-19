70ecffaea78e66aee9094d15d287cb4c978d58bb
safikhanSoofiyani <safikhan2000@gmail.com>
2024-06-19 11:29:14 +0530

    modified compare vanilla

--- llm_evaluators/compare_vanilla.py
+++ llm_evaluators/compare_vanilla.py
@@ -54,14 +54,22 @@ def create_dict(id, question, answer_a, answer_b):
     } 
     return orig_dict 
 
-def process_instance(data_row):
-    id = data_row['cdx']
-    prompt = data_row['question']
-    original_answer = data_row['og']
-    perturbed_answer = data_row['perturbed_gpt4']
+def process_instance(data_row, p_mode):
+    if p_mode:
+        id = data_row['cdx']
+        prompt = data_row['question']
+        original_answer = data_row['perturbed_gpt4']
+        perturbed_answer = data_row['og']
+        orig_id = f"{id}~compare_vanilla~perturb_orig"
+    else:        
+        id = data_row['cdx']
+        prompt = data_row['question']
+        original_answer = data_row['og']
+        perturbed_answer = data_row['perturbed_gpt4']
+        orig_id = f"{id}~compare_vanilla~orig_perturb"
     
     #processing the original answer
-    orig_id = f"{id}~compare_vanilla~orig_perturb"
+    
     orig_dict = create_dict(orig_id, prompt, original_answer, perturbed_answer)
     
     return [orig_dict] 
@@ -75,6 +83,7 @@ def parse_args():
     parser.add_argument("--out_file_name", type=str, help="Output jsonl File name")
     parser.add_argument("--batch_mode", action="store_true", default=True, help="Run evaluation in batch mode")
     parser.add_argument("--model", type=str, choices=['gpt-4o', "gpt-4-turbo", "gpt-3.5-turbo-0125", "llama3-70b", "claude3-opus", 'gemini-1.5-flash', 'gemini-1.5-pro'], help="Model name")
+    parser.add_argument("--p_mode", action="store_true", default=False, help="Run in perturbed first mode")
     args = parser.parse_args()
     return args
 
@@ -89,9 +98,13 @@ def main(args):
         if args.batch_mode:
             final_jsonl = []
             for row in df_dict:
-                row_dicts = process_instance(row)
+                row_dicts = process_instance(row, args.p_mode)
                 final_jsonl.extend(row_dicts)
-                write_jsonl(final_jsonl, args.out_file_name)
+                if args.p_mode:
+                    out_file_name = args.out_file_name.split(".json")[0] + "_perturb.jsonl"
+                    write_jsonl(final_jsonl, out_file_name)
+                else:
+                    write_jsonl(final_jsonl, args.out_file_name)
         else:
             print("2Still pending")
     else:
